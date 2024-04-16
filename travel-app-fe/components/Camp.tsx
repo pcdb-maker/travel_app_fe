168d292dca30782f27fc01842cb2edec0ce96684
22415327-scu <chloeprofessionaldevelopment@outlook.com>
2024-04-16 14:20:51 +1000

    Container section added to camp.tsx

--- travel-app-fe/components/Camp.tsx
+++ travel-app-fe/components/Camp.tsx
@@ -47,23 +47,7 @@ const CampSite = ({ backgroundImage, title, subtitle, peopleJoined }: CampProps)
   )
 }
 
-const Camp = () => {
-  return (
-    <section className="2xl:max-container relative flex flex-col py-10 lg:mb-10 lg:py-20 xl:mb-20">
-      <div className="hide-scrollbar flex h-[340px] w-full items-start justify-start gap-8 overflow-x-auto lg:h-[400px] xl:h-[640px]">
-        <CampSite 
-          backgroundImage="bg-bg-img-1"
-          title="Putuk Truno Camp"
-          subtitle="Prigen, Pasuruan"
-          peopleJoined="50+ Joined"
-        />
-        <CampSite 
-          backgroundImage="bg-bg-img-2"
-          title="Mountain View Camp"
-          subtitle="Somewhere in the Wilderness"
-          peopleJoined="50+ Joined"
-        />
-      </div>
+
 
       <div className="flexEnd mt-10 px-6 lg:-mt-60 lg:mr-6">
         <div className="bg-green-50 p-8 lg:max-w-[500px] xl:max-w-[734px] xl:rounded-5xl xl:px-16 xl:py-20 relative w-full overflow-hidden rounded-3xl">
